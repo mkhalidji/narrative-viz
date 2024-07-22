86a1b7c273765939fcfab54dd375d36e331e01aa
Mojtaba Khaliji <mkhalidji@gmail.com>
2024-07-21 19:00:22 -0500

    created simple choropleth

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,34 +53,99 @@ window.onload = async function () {
   const height = 610;
 
   const us = await d3.json("data/counties-albers-10m.json");
-  console.log(us);
+  let covidData;
+  try {
+    covidData = await d3.csv(
+      "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
+    );
+  } catch (e) {
+    console.log(e);
+  }
+
+  const getCovidDataForDate = (data, targetDate) => {
+    return Object.assign(
+      {},
+      ...data
+        .filter(({ date }) => date === targetDate)
+        .map(({ state, cases, deaths }) => ({
+          state,
+          cases: parseInt(cases),
+          deaths: parseInt(deaths),
+        }))
+        .map((d) => ({ [d.state]: { ...d } }))
+    );
+  };
+
+  const covidDataYesterday = getCovidDataForDate(covidData, "2020-04-15");
+  const covidDataToday = getCovidDataForDate(covidData, "2021-10-10");
+  covidData = Object.assign(
+    {},
+    ...Object.keys(covidDataToday).map((state) => {
+      if (!covidDataToday[state] || !covidDataYesterday[state]) {
+        return {};
+      }
+      const cases =
+        covidDataToday[state].cases - covidDataYesterday[state].cases;
+      const deaths =
+        covidDataToday[state].deaths - covidDataYesterday[state].deaths;
+      const rate = (
+        cases > 0 && deaths > 0 ? (deaths / cases) * 100.0 : 0.0
+      ).toFixed(2);
+      return { [state]: { state, cases, deaths, rate } };
+    })
+  );
+  console.log(covidData);
+
+  const upperLimit = Math.floor(
+    Math.max(...Object.values(covidData).map(({ rate }) => rate))
+  );
+  console.log(upperLimit);
+
+  const casesColor = d3
+    .scaleDivergingPow([0.5, 1.0, 1.5], ["#22763f", "#f4cf64", "#be2a3e"])
+    .clamp(true);
 
   const zoom = d3.zoom().scaleExtent([1, 8]);
 
   const svg = d3
     .select("svg")
     .attr("viewBox", [0, 0, width, height])
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", "100%")
+    .attr("height", "auto");
 
   const path = d3.geoPath();
 
   const g = svg.append("g");
 
   const states = g
-    .append("g")
-    .attr("fill", "#4453")
     .attr("cursor", "pointer")
-    .selectAll("path")
+    .attr("fill", "#4453")
+    .selectAll("g")
     .data(topojson.feature(us, us.objects.states).features)
-    .join("path")
-    .attr("d", path);
+    .join("g");
 
-  states.append("title").text((d) => d.properties.name);
+  g.selectAll("g").attr("fill", ({ properties: { name: state } }) => {
+    return casesColor(covidData[state].rate);
+  });
+
+  states
+    .append("path")
+    .attr("d", path)
+    .append("title")
+    .text(({ properties: { name: state } }) => {
+      const { cases, deaths, rate } = covidData[state];
+
+      return `${state}\nCases: ${cases}\nDeaths: ${deaths}\nRate: ${rate}%`;
+    });
 
   g.append("path")
     .attr("fill", "none")
-    .attr("stroke", "lightgrey")
+    .attr("stroke", "#a2a2a2")
     .attr("stroke-linejoin", "round")
     .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
+
+  g.append("path")
+    .attr("fill", "none")
+    .attr("stroke", "#a2a2a2")
+    .attr("d", path(topojson.mesh(us, us.objects.nation)));
 };
